210bd9b01b8b48ca62205fe60c40ac996b2427bf
Gustavo Teixeira <contact@gustavoteixeira.dev>
2024-01-07 02:14:04 -0300

    Refactor readme, update server code, and enhance auth configuration
    This commit includes instructions simplification in the README file. In
    the server code, in src/hooks.server.ts, the auth configuration is
    updated where multiple environment variables are merged into a single
    command. Google and GitHub authentication is enhanced by updating client
    secret values and adding auth secret. Additionally, Sveltekit's
    adapter-node is added to the package.json dependencies list to inform
    the target environment. The server.js is a new file for handling the
    server-side related activities, and it uses dotenv for managing
    environment variables.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,11 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/sveltekit/providers/github';
 import Google from '@auth/sveltekit/providers/google';
+import type { Handle } from '@sveltejs/kit';
+import { GOOGLE_CLIENT_ID,
+	GOOGLE_CLIENT_SECRET, AUTH_SECRET, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
 
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
-import {
-	GOOGLE_CLIENT_ID,
-	GOOGLE_CLIENT_SECRET
-} from '$env/static/private';
-
-export const handle = SvelteKitAuth({
+export const handle: Handle = SvelteKitAuth({
 	providers: [GitHub({
 		clientId: GITHUB_CLIENT_ID,
 		clientSecret: GITHUB_CLIENT_SECRET
@@ -17,5 +14,6 @@ export const handle = SvelteKitAuth({
 	pages: {
 		signIn: '/login',
 		signOut: '/login'
-	}
+	},
+	secret: AUTH_SECRET
 });
